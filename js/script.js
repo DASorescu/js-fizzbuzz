adf1c8705817a9fc81ae0da673a6d5557e717ee2
github_DASorescu <srsdln97@gmail.com>
2022-06-23 13:55:11 +0200

    linked-js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// definire le variabili che mi servono 
+// ciclo for per stampare numeri fino a che arrivo a 100
+// gestire con il ciclo if else se un numero e divisibile per 3 per 5 o per entrambi 
+// cambiare in quel caso l'output a seconda se e divisibile per 3 per 5 o entrambi 
+
+// #definisco le variabili da usare per i criteri di divisibilita'
+const div5 = 5;
+const div3 = 3;
+let numString = "";
+// # ciclo for 
+for( i = 1 ; 1 <= 100 ; i++){
+numString += " "i ;
+
+}
+
+console.log(numString)
