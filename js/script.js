b1449fbc35807f1c9876a753c39b04a5edbc7f28
github_DASorescu <srsdln97@gmail.com>
2022-06-23 15:44:51 +0200

    ADDED-BONUS-GRAPHICS

--- js/script.js
+++ js/script.js
@@ -6,23 +6,25 @@
 // #definisco le variabili da usare per i criteri di divisibilita'
 const div5 = 5;
 const div3 = 3;
-let numString = "";
-
+let numString = "<ul>";
+const blackboard = document.getElementById("board")
 // # ciclo for 
 for(let i = 1 ; i <= 100 ; i++){
     
     // condizione di divisibilita'
     if  ((i % div3 == 0) && (i % div5 == 0)){
-        numString += " FizzBuzz" ;
+        numString += `<li id="fb">FizzBuzz</li>`  ;
     } else if (i % div5 == 0){
-        numString += " buzz"
+        numString += `<li id="b">Fizz</li>`
     } else if(i % div3 == 0 ){
-        numString += " Fizz"
+        numString += `<li id="f">Buzz</li>`
     }else{
-        numString += " "+ i  ;
+        numString += `<li>${i}</li>`   ;
     }
 
     
 }
-
+numString += "</ul>";
 console.log(numString);
+
+blackboard.innerHTML = numString;
