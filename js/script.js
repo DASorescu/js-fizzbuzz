dc8a6c1cfa1c8434c8e056c2d24fa7a3d447a236
github_DASorescu <srsdln97@gmail.com>
2022-06-23 14:37:55 +0200

    EXERCISE-COMPLETED

--- js/script.js
+++ js/script.js
@@ -7,10 +7,22 @@
 const div5 = 5;
 const div3 = 3;
 let numString = "";
-// # ciclo for 
-for( i = 1 ; 1 <= 100 ; i++){
-numString += " "i ;
 
+// # ciclo for 
+for(let i = 1 ; i <= 100 ; i++){
+    
+    // condizione di divisibilita'
+    if  ((i % div3 == 0) && (i % div5 == 0)){
+        numString += " FizzBuzz" ;
+    } else if (i % div5 == 0){
+        numString += " buzz"
+    } else if(i % div3 == 0 ){
+        numString += " Fizz"
+    }else{
+        numString += " "+ i  ;
+    }
+
+    
 }
 
-console.log(numString)
+console.log(numString);
